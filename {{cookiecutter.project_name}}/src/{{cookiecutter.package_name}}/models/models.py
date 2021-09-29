284449b9f54f5577a7f3d8b25660a8cbe29c48e0
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-09-29 10:30:07 -0400

    docs: updated readme for installation/dev instructions

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/models/models.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/models/models.py
@@ -7,7 +7,10 @@ ready to training/usage, based off of layers defined in
 either `torch`, other packages, or in `{{cookiecutter.package_name}}.layers`.
 """
 
+import torch
+from torch import Tensor      # this is used for type annotations
 from torch import nn
 import wandb
 import pytorch_lightning as pl
 
+from {{cookiecutter.package_name}} import layers
