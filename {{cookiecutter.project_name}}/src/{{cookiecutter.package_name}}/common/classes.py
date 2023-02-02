0d14ff346c2141a54bce24607b9b36f72fda2a41
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-02-01 21:21:28 -0800

    feat & docs: added more convenience stuff and docs to data structure

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/common/classes.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/common/classes.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from abc import ABC, abstractproperty, abstractstaticmethod
 from dataclasses import dataclass
-from typing import Any, Iterable
+from typing import Any, Iterable, Optional
+
+from torch import Tensor
 
 
 @dataclass
@@ -20,11 +22,39 @@ class AbstractDataStructure(ABC):
     the key/data values you would expect.
     """
 
-    def __getitem__(self, key: str) -> Any:
-        return getattr(self, key)
+    def __getitem__(self, key: str, default: Optional[Any] = None) -> Any:
+        return getattr(self, key, default)
 
     def __setitem__(self, key: str, value: Any):
         setattr(self, key, value)
+
+
+    def get(self, key: str, default: Optional[Any] = None) -> Any:
+        """
+        User facing method for retrieving data, mimicking how dictionaries
+        function.
+
+        Parameters
+        ----------
+        key : str
+            Key to retrieve from the structure.
+        default : Optional[Any]
+            Optional value to set as the default, if the key
+            exists but doesn't contain data.
+
+        Returns
+        -------
+        Any
+            Data to be retrieved
+
+        Raises
+        ------
+        KeyError:
+            If `key` is not an attribute of this data structure.
+        """
+        if key not in dir(self):
+            raise KeyError(f"{key} is not found in {self.__class__.__name__}.")
+        return self.__getitem__(key, default)
 
     @property
     def is_batched(self) -> bool:
@@ -59,3 +89,24 @@ class AbstractDataStructure(ABC):
             Number of samples within the batch.
         """
         ...
+
+    @property
+    def is_not_finite(self) -> bool:
+        """
+        Convenient property to check if any tensors
+        contained within this data structure has inf
+        and/or nans.
+
+        Returns
+        -------
+        bool
+            True if there are any elements within the 
+            structure that is not finite, else False.
+        """
+        for key in dir(self):
+            attr = self.get(key)
+            if isinstance(attr, Tensor):
+                has_na = ~attr.isfinite().any()
+                if has_na:
+                    return True
+        return False
