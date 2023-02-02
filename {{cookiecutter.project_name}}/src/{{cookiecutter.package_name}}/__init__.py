412607da057c369959de919421698da4d55d87bc
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-02-01 21:44:52 -0800

    refactor: moved utils to common submodule

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,6 +1,6 @@
 """{{cookiecutter.friendly_name}}."""
 
-from {{cookiecutter.package_name}}.utils import get_paths
+from {{cookiecutter.package_name}}.common.utils import get_paths
 from {{cookiecutter.package_name}} import models, pipeline, layers
 
 __version__ = "0.1.0"
