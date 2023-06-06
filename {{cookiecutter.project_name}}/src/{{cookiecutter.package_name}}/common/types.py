8bd353f0ffb8249757ab4dabde42c5aeb269c54a
Lee, Kin Long Kelvin <kin.long.kelvin.lee@gmail.com>
2023-06-06 21:51:02 +1000

    added optional layer annotation
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@gmail.com>

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/common/types.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/common/types.py
@@ -1,8 +1,11 @@
 
-from typing import Union, Dict
+from typing import Union, Dict, Optional, Type
 
-from torch import Tensor
+from torch import Tensor, nn
 
 # addes convenient types for annotation
 DataType = Union[float, int, Tensor]
 InputType = Union[Dict[str, DataType], DataType]
+
+# types that are optional types/modules
+OptionalLayer = Optional[Union[nn.Module, Type[nn.Module]]]
